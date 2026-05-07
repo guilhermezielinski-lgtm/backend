8cee5dbedd4a0d3169c4b1a8d3a62217c4e54937
guilhermezielinski-lgtm <guilherme.zielinski@escola.pr.gov.br>
2026-05-07 08:03:49 -0300

    Create ex023.py

---
+++ exercicios/ex023.py
@@ -0,0 +1,12 @@
+num = int(input('Digite um número (0 a 9999): '))
+
+# Isolando os dígitos matematicamente
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milha: {m}')
